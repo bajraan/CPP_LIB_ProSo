96056a7d0df5d8bbe0d6bcfdd685eaacf8bf90af
Pawel Baran <111999589+bajraan@users.noreply.github.com>
2023-07-21 13:57:59 +0200

    Formating

--- 00_pbaProSo/pbaProSo.cpp
+++ 00_pbaProSo/pbaProSo.cpp
@@ -61,12 +61,10 @@ std::vector<int> intigerToDigitsVector(int n) {
 int intigerToIntigerReversed(int n) {
 
     int reversed = 0;
-
     while (n != 0) {
         reversed = reversed * 10 + n % 10;
         n /= 10;
     }
-
     return reversed;
 }
 
